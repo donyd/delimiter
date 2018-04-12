db19b3c4d358dbfa5465e3c6dc4bd29dcf7dd5d8
= <bonebare@gmail.com>
2018-04-12 15:11:02 +0100

    refactored layout into app class

--- appFrame.py
+++ appFrame.py
@@ -1,17 +1,32 @@
 import tkinter as tk
+from tkinter import *
 
 
-app = tk.Tk()
-app.size("400 x 700")
-
-app.mainloop()
-
-
-## Classes
-class Delimiter:
+class App:
 
     def __init__(self):
-        return self
+        # Main Frame Creation
+        self.master = Tk()
+        self.master.title("Delimiter Prototype")
+
+        # Labels & Placement
+        self.columnarLbl = tk.Label(text="In: Column ")
+        self.columnarLbl.grid(column=0, row=0)
+
+        self.linearLbl = tk.Label(text="Out: CSV")
+        self.linearLbl.grid(column=1, row=0)
+
+        # Text Area Boxes
+        self.colOne = tk.Text(self.master, height=25, width=40)
+        self.colOne.grid(column=0, row=1)
+
+        self.colTwo = tk.Text(self.master, height=25, width=40)
+        self.colTwo.grid(column=1, row=1)
+
+        # Buttons
+        self.btnConvert = Button(self.master, text="Convert")
+
+        self.master.mainloop()
 
 
-class 
+App()

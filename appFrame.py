9786bc18a7d864cd20bb034ea781d23e6169d997
= <bonebare@gmail.com>
2018-04-20 17:31:53 +0100

    fixed trailing new lines and cleaned up code

--- appFrame.py
+++ appFrame.py
@@ -9,7 +9,6 @@ class App:
         # Main Frame Creation
         self.master = Tk()
         self.master.title("Delimiter Prototype")
-        self.csv = []
         self.lstStr = '';
 
         # Labels & Placement
@@ -17,7 +16,7 @@ class App:
         self.columnarLbl.grid(column=0, row=0)
 
         self.linearLbl = tk.Label(text="Out: CSV")
-        self.linearLbl.grid(column=1, row=0)
+        self.linearLbl.grid(column=2, row=0)
 
         # Paste
         def leftpaste(event):
@@ -26,6 +25,7 @@ class App:
         # Clear
         def clear():
             self.colOne.delete('1.0', END)
+            self.colTwo.delete('1.0', END)
 
         # Text Area Boxes
         self.colOne = tk.Text(self.master, height=25, width=40)
@@ -33,37 +33,56 @@ class App:
         self.colOne.grid(column=0, row=1)
 
         self.colTwo = tk.Text(self.master, height=25, width=40)
-        self.colTwo.grid(column=1, row=1)
+        self.colTwo.grid(column=2, row=1)
 
         # Convert function
         def convert():
             self.colTwo.delete('1.0', END)
+            # Clear memory on each session
             self.lstStr = ''
+
             temp = self.colOne.get('1.0', END).split('\r\n')
             # temp = self.colOne.get('1.0', 'end-1c')
             print(temp)
-            print(len(temp))
-            length = len(temp) - 1
+            #print(len(temp))
 
-            for i in range(length):
+            temp = list(filter(listChecker, temp))
+            print('Post invalid item check')
+            print(temp)
+
+            for i in range(len(temp)):
                 self.lstStr += temp[i] + ', '
 
-            self.lstStr = self.lstStr.lstrip(', ')
 
-            self.lstStr = self.lstStr.strip()
+            self.lstStr = self.lstStr.rstrip('\n')
+            self.lstStr = self.lstStr.rstrip(' ,')
 
-            self.colTwo.insert(END, self.lstStr.rstrip(', '))
-            print(self.lstStr)
+            self.colTwo.insert(END, self.lstStr)
 
+        # Filter Function
+        def listChecker(list):
+            invalidItems = ['\n', '', ' ']
+
+            if(list in invalidItems):
+                return False
+            else:
+                return True
 
         # Buttons
+        self.btnColumnar = Button(self.master, text="Columnar Data")
+        self.btnColumnar.grid(column=0, row=2)
+
         self.btnClear = Button(self.master, text="Clear", command=clear)
-        self.btnClear.grid(column=0, row=2)
+        self.btnClear.grid(column=1, row=2)
 
 
-        self.btnConvert = Button(self.master, text="Convert", command=convert)
-        self.btnConvert.grid(column=1, row=2)
+        self.btnConvert = Button(self.master, text="Create CSV", command=convert)
+        self.btnConvert.grid(column=2, row=2)
 
+        self.chkOneClick = Checkbutton(self.master, text="Click Less")
+        self.chkOneClick.grid(column=1, row=1)
+
+        # Start yer Engines
         self.master.mainloop()
 
 
--- test.py
+++
@@ -1,6 +0,0 @@
-numLst = [111111, 222222, 333333, 444444, 555555]
-
-for i in range(len(numLst)):
-               print (numLst[i])
-               
-

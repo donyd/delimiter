126c72a44ee14142e5232b036f9d9b916932a341
Donal <DDsilva@dedsert.local>
2018-05-11 14:50:56 +0100

    added auto paste and copy option

--- appFrame.py
+++ appFrame.py
@@ -9,7 +9,8 @@ class App:
         # Main Frame Creation
         self.master = Tk()
         self.master.title("Delimiter Prototype")
-        self.lstStr = '';
+        self.lstStr = ''
+        self.checkLessState = IntVar()
 
         # Labels & Placement
         self.columnarLbl = tk.Label(text="In: Column ")
@@ -20,7 +21,13 @@ class App:
 
         # Paste
         def leftpaste(event):
-            self.colOne.insert(END, pyperclip.paste())
+            if(self.checkLessState.get()):
+                self.colOne.insert(END, pyperclip.paste())
+
+        # Copy
+        def leftcopy(event):
+            if(self.checkLessState.get()):
+                pyperclip.copy(self.colTwo.get('1.0', END))
 
         # Clear
         def clear():
@@ -30,9 +37,11 @@ class App:
         # Text Area Boxes
         self.colOne = tk.Text(self.master, height=25, width=40)
         self.colOne.bind("<Button-1>", leftpaste)
+        print(self.checkLessState)
         self.colOne.grid(column=0, row=1)
 
         self.colTwo = tk.Text(self.master, height=25, width=40)
+        self.colTwo.bind("<Button-1>", leftcopy)
         self.colTwo.grid(column=2, row=1)
 
         # Convert function
@@ -44,7 +53,6 @@ class App:
             temp = self.colOne.get('1.0', END).split('\r\n')
             # temp = self.colOne.get('1.0', 'end-1c')
             print(temp)
-            #print(len(temp))
 
             temp = list(filter(listChecker, temp))
             print('Post invalid item check')
@@ -79,7 +87,8 @@ class App:
         self.btnConvert = Button(self.master, text="Create CSV", command=convert)
         self.btnConvert.grid(column=2, row=2)
 
-        self.chkOneClick = Checkbutton(self.master, text="Click Less")
+        self.chkOneClick = Checkbutton(self.master, text="Click Less", variable=self.checkLessState)
+        print(self.checkLessState)
         self.chkOneClick.grid(column=1, row=1)
 
         # Start yer Engines
